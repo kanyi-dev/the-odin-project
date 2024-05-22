6a71cffb9d405d5e3646cf24f0c8645f2888d9a8
kanyi-maina <170375642+kanyi-maina@users.noreply.github.com>
2024-05-22 17:17:18 +0300

    two separate methods

---
+++ objects/design.js
@@ -0,0 +1,18 @@
+// example 1
+
+const playerOneName = "tim";
+const playerTwoName = "jenn";
+const playerOneMarker = "X";
+const playerTwoMarker = "O";
+
+//Example 2
+
+const playerOne = {
+    name: "tim",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "jenn",
+    marker: "O"
+};
